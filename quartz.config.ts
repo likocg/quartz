9cb37904fab25f433bcad67bcfd0aa01435eba33
likocg <88356675+likocg@users.noreply.github.com>
2025-07-11 13:28:50 -0300

    erro no config

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,7 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      { provider: 'google', tagId: 'G-1QMYQKJCCD' },
+      provider: 'google', tagId: 'G-1QMYQKJCCD',
     },
     locale: "pt-BR",
     baseUrl: "likocg.github.io/quartz",
